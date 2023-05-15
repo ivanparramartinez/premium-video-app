988b0f9b4c7b4e55ebec0cf18fbb6cdb1cb631a1
Ivan Parra <niparrama@unal.edu.co>
2023-05-15 04:27:17 -0500

    Added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { onBeforeMount, ref, watch } from 'vue'
 import axios from 'axios'
 import CompMovieCard from '@/components/CompMovieCard.vue'
 
-const uri = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDB_API_KEY}&`
+const uri = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDB_API_KEY}&`
 
 const search = ref('')
 const movies = ref([])
